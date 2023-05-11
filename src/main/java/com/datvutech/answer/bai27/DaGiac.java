2cc38f39c339e3640622198a5f83554dab6ff485
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-05-11 09:05:28 +0700

    arranged files

--- src/main/java/com/datvutech/answer/bai27/DaGiac.java
+++ src/main/java/com/datvutech/answer/bai27/DaGiac.java
@@ -1,9 +1,9 @@
 package com.datvutech.answer.bai27;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 import com.datvutech.answer.bai24.Diem2D;
+import com.datvutech.util.AppScanner;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,26 +11,84 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DaGiac {
-    /* #: Properties */
-    private List<Diem2D> diems;
 
-    /* # Properties */
-    /* #: Constructors */
+    // fields
+    private int soCanh;
+    private Diem2D[] diems;
 
-    public DaGiac(Diem2D diem1, Diem2D diem2, Diem2D diem3) {
-        diems = new ArrayList<>();
-        diems.add(diem1);
-        diems.add(diem2);
-        diems.add(diem3);
+    // constructors
+    public DaGiac() {
     }
-    /* # Constructor */
 
-    /* #: Input, output */
+    public DaGiac(int soCanh, Diem2D[] diems) {
+        this.soCanh = soCanh;
+        this.diems = diems;
+    }
+
+    public DaGiac(DaGiac dg) {
+        soCanh = dg.getSoCanh();
+        diems = dg.getDiems();
+    }
+
+    // getters, setters
+    public int getSoCanh() {
+        return soCanh;
+    }
+
+    public void setSoCanh(int soCanh) {
+        this.soCanh = soCanh;
+    }
+
+    public Diem2D[] getDiems() {
+        return diems;
+    }
+
+    public void setDiems(Diem2D[] arr) {
+        this.diems = arr;
+    }
+
+    // business code
     public void nhap() {
-        Diem2D diem2d = new Diem2D(0, 0);
-        diem2d.nhapDiem();
-        diems.add(diem2d);
+        Scanner sysScanner = AppScanner.getSystemScanner();
+        do {
+            System.out.print("soCanh = ");
+            soCanh = sysScanner.nextInt();
+        } while (soCanh <= 2);
+
+        diems = new Diem2D[soCanh];
+        for (int i = 0; i < soCanh; i++) {
+            System.out.println("Nhap canh thu " + (i + 1) + " = ");
+            Diem2D diem = new Diem2D(0, 0);
+            diem.nhapDiem();
+            diems[i] = diem;
+        }
     }
 
-    /* # Input, output */
+    public void xuat() {
+        for (int i = 0; i < soCanh; i++) {
+            System.out.print(diems[i]);
+        }
+    }
+
+    public int tinhChuVi() {
+        int cv = 0;
+        for (int i = 0; i < soCanh - 1; i++) {
+            cv += diems[i].khoangCach(diems[i + 1]);
+        }
+        return cv;
+    }
+
+    public Diem2D dinhXaNhat() {
+        Diem2D dinh = diems[0];
+        float xaNhat = diems[0].khoangCach();
+
+        for (int i = 1; i < soCanh; i++) {
+            float temp = diems[i].khoangCach();
+            if (temp > xaNhat) {
+                xaNhat = temp;
+                dinh = diems[i];
+            }
+        }
+        return dinh;
+    }
 }
